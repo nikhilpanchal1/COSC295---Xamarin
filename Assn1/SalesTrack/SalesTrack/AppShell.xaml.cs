e72611be6f396681afa8dff98cf8f69c25974416
Nikhil Panchal <37629345+nikhilpanchal1@users.noreply.github.com>
2023-03-18 17:56:15 -0600

    Initial commit

---
+++ Assn1/SalesTrack/SalesTrack/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SalesTrack.ViewModels;
+using SalesTrack.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SalesTrack
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Assn1/SalesTrack/SalesTrack/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SalesTrack.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SalesTrack.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
