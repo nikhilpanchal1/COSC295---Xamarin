b48536b6277de1b2bb9e185c4472abb7fbacf437
Nikhil Panchal <37629345+nikhilpanchal1@users.noreply.github.com>
2023-03-19 20:05:27 -0600

    Fixed one more bug, Interactions page is still missing

--- Assn1/SalesTrack/SalesTrack/Views/AddEditCustomerPage.xaml.cs
+++ Assn1/SalesTrack/SalesTrack/Views/AddEditCustomerPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SalesTrack.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +16,19 @@ namespace SalesTrack.Views
         {
             InitializeComponent();
         }
+        private async void SaveClicked(object sender, EventArgs e)
+        {
+            // Get the binding context of the page, which should be an instance of AddEditCustomerViewModel
+            var viewModel = (AddEditCustomerViewModel)BindingContext;
+
+            // Execute the SaveCommand to save the customer data to the database
+            viewModel.SaveCommand.Execute(null);
+
+            // Navigate back to the previous page
+            await Shell.Current.Navigation.PopAsync();
+        }
+
+
+
     }
 }
