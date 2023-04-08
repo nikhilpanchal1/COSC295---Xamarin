ed1fa249c756b4c1d13c5ac558ec9b39f9cde027
Nikhil Panchal <37629345+nikhilpanchal1@users.noreply.github.com>
2023-04-07 18:02:51 -0600

    Toggle

---
+++ Assn1/SalesTrack2/SalesTrack/SalesTrack/Data/PurchaseToggle.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace SalesTrack.Data
+{
+    public class PurchaseToggle : Switch
+    {
+        public PurchaseToggle()
+        {
+            Toggled += OnToggled;
+        }
+
+        private void OnToggled(object sender, ToggledEventArgs e)
+        {
+            // Handle the toggling of the Purchased property
+            // You may need to modify this to update the database or the ViewModel.
+        }
+    }
+}
